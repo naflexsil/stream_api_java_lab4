e470633d602ef26b0ad7304c839d0edaf5f2033d
a_лина <118667639+naflexsil@users.noreply.github.com>
2024-11-08 16:18:28 +0700

    technic

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,14 +5,26 @@ plugins {
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation ("com.google.code.gson:gson:2.10.1")
+    compileOnly ("org.projectlombok:lombok:1.18.34")
+    annotationProcessor ("org.projectlombok:lombok:1.18.34")
+    testImplementation ("org.junit.jupiter:junit-jupiter-api:5.11.1")
+    testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.11.1")
 }
+
+/* application {
+    mainClass = "org.example.Main"
+} */
 
 tasks.test {
     useJUnitPlatform()
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,98 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.*;
+import lombok.Data;
+
+@Data
+class Visitor {
+    private String name;
+    private String surname;
+    private String phone;
+    private boolean subscribed;
+    private List<Book> favoriteBooks;
+}
+
+@Data
+class Book {
+    private String name;
+    private String author;
+    private int publishingYear;
+    private String isbn;
+    private String publisher;
+}
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("type 1 for count visitor or 2 for list unique books or 3 for:");
+        String choice = scanner.nextLine();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Gson gson = new Gson();
+        Type visitorListType = new TypeToken<List<Visitor>>() {}.getType();
+
+        try (FileReader reader = new FileReader("books.json")) {
+            List<Visitor> visitors = gson.fromJson(reader, visitorListType);
+
+            if ("1".equals(choice)) {
+                displayVisitors(visitors);
+            } else if ("2".equals(choice)) {
+                displayUniqueFavoriteBooks(visitors);
+            } else if ("3".equals(choice)) {
+                displayBooksSortedByYear(visitors);
+            } else {
+                System.out.println("error choice");
+            }
+        } catch (IOException e) {
+            System.out.println("error reading file: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private static void displayVisitors(List<Visitor> visitors) {
+        System.out.println("Count visitor: " + visitors.size());
+        System.out.println("List visitor:");
+
+        for (Visitor visitor : visitors) {
+            System.out.println(visitor.getName() + " " + visitor.getSurname());
+            System.out.println("Phone number: " + visitor.getPhone());
+            System.out.println("Favorite books:");
+            for (Book book : visitor.getFavoriteBooks()) {
+                System.out.println(" - " + book.getName() + " (author: " + book.getAuthor() + ")");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void displayUniqueFavoriteBooks(List<Visitor> visitors) {
+        Set<Book> uniqueBooks = new HashSet<>();
+        for (Visitor visitor : visitors) {
+            uniqueBooks.addAll(visitor.getFavoriteBooks());
+        }
+
+        System.out.println("Count unique favorite books: " + uniqueBooks.size());
+        System.out.println("List unique books:");
+        for (Book book : uniqueBooks) {
+            System.out.println(" - " + book.getName() + " (author: " + book.getAuthor() + ")");
+        }
+    }
+
+    private static void displayBooksSortedByYear(List<Visitor> visitors) {
+        List<Book> allBooks = new ArrayList<>();
+        for (Visitor visitor : visitors) {
+            allBooks.addAll(visitor.getFavoriteBooks());
+        }
+
+        allBooks.sort(Comparator.comparingInt(Book::getPublishingYear));
+
+        System.out.println("List of books sorted by year of publication:");
+        for (Book book : allBooks) {
+            System.out.println(book.getPublishingYear() + " - " + book.getName() + " (author: " + book.getAuthor() + ")");
         }
     }
 }
